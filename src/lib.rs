411e642ea38e2b82431ccd164463c9853f773efd
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-24 12:11:16 +0900

    wasmでcanvasに三角形を描画

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,28 @@ pub fn main_js() -> Result<(), JsValue> {
     console::log_1(&JsValue::from_str("Hello world!"));
     console::info_1(&JsValue::from_str("unko"));
 
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.move_to(300.0, 0.0);
+    context.begin_path();
+    context.line_to(0.0, 600.0);
+    context.line_to(600.0, 600.0);
+    context.line_to(300.0, 0.0);
+    context.close_path();
+    context.stroke();
+    context.fill();
+
     Ok(())
 }
